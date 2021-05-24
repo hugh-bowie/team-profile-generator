499cdd33b0e40092168210b6aff682fd20789820
Hugh Bowie IV <hughbowie@me.com>
2021-05-23 22:18:14 -0400

    Update Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,15 @@
-any
+const Employee = require("../lib/Employee");
+const testEmployee = new Employee('Vanessa', 1, 'vfatta@yahoo.com')
+
+test('name as entered', () => {
+    expect(testEmployee.name).toEqual(expect.any(String))
+    expect(testEmployee.name.length).toBeGreaterThan(2)
+})
+
+test('ID is any number', () => {
+    expect(testEmployee.id).toEqual(expect.any(Number))
+})
+
+test('email is valid', () => {
+    expect(testEmployee.email).toEqual(expect.stringContaining('@'))
+})
