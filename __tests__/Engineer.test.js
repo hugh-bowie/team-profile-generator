c191b19d7925d9ae8e825bf4affdd8d9a06ae46f
Hugh Bowie IV <hughbowie@me.com>
2021-05-23 22:23:31 -0400

    Update Engineer.test.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,24 +1,24 @@
-const Engineer = require('../lib/Engineer.js')
+const Engineer = require('../lib/Engineer.js');
 
-const testEngineer = new Engineer('Hugh', 1, 'hughbowie@me.com', 'engineer', 'hugh-bowie')
+const testEngineer = new Engineer('Hugh', 1, 'hughbowie@me.com', 'engineer', 'hugh-bowie');
 
 test('name as entered', () => {
-    expect(testEngineer.name).toEqual(expect.any(String))
-    expect(testEngineer.name.length).toBeGreaterThan(2)
+    expect(testEngineer.name).toEqual(expect.any(String));
+    expect(testEngineer.name.length).toBeGreaterThan(2);
 })
 
 test('ID is any number', () => {
-    expect(testEngineer.id).toEqual(expect.any(Number))
+    expect(testEngineer.id).toEqual(expect.any(Number));
 })
 
 test('email is valid', () => {
-    expect(testEngineer.email).toEqual(expect.stringContaining('@'))
+    expect(testEngineer.email).toEqual(expect.stringContaining('@'));
 })
 
 test('is role Engineer', () => {
-    expect(testEngineer.role).toBe('engineer')
+    expect(testEngineer.role).toBe('engineer');
 })
 
 test('Github was provided', () => {
-    expect(testEngineer.gitHub).toEqual(expect.any(String))
+    expect(testEngineer.gitHub).toEqual(expect.any(String));
 })
